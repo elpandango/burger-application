8244c09e87b2a7bf83d1940aa9a34d0693c75129
Spider <spider.netpeak@gmail.com>
2018-03-22 14:58:13 +0200

    Logo responsive / Navigation added

--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import classes from './Toolbar.css';
 import Logo from '../../Logo/Logo';
+import NavigationItems from '../NavigationItems/NavigationsItems';
 
 const toolbar = (props) => (
     <header className={classes.Toolbar}>
         <div>MENU</div>
-        <Logo/>
+        <div className={classes.Logo}>
+            <Logo />
+        </div>
         <nav>
-            ...
+           <NavigationItems/>
         </nav>
     </header>
 );
